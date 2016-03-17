80745b93ebeafb0c265e0ab3ab6237aa33c2abd1
Ubuntu <ubuntu@ip-172-31-44-227.us-west-2.compute.internal>
2016-03-17 19:47:13 +0000

    make it better

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-var express = require('express')
-var app = express()
+var fs = require('fs')
+var http = require('http')
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+var data = fs.readFileSync('index.html')
 
-app.get('/', function(request, response) {
-  response.sendfile('index.html')
-})
+function onRequest(request, response) {
+  response.writeHead(200, {"Content-Type": "html"})
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+  response.write(data)
+  response.end()
+}
+
+http.createServer(onRequest).listen(process.env.PORT || 8080)
 
