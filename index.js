a5512dda855e38703cb23f5224aea52e2fd5ed04
Ubuntu <ubuntu@ip-172-31-44-227.us-west-2.compute.internal>
2016-03-17 08:17:08 +0000

    make it better

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
-var express = require('express')
-var app = express()
+var http = require('http');
+var requestListener = function (req, res) {
+  res.writeHead(200);
+  res.end('Hello, World!\n');
+}
 
-/* serves all the static files*/
-app.set('port', (process.env.PORT || 8080))
-app.use(express.static(__dirname + '/public'))
+var server = http.createServer(requestListener);
+server.listen(8080);
 
-/*serves main page*/
-app.get('/', function(request, response) {
-response.sendfile('index.html') 
-})
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
