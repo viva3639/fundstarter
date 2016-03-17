e6339a1db9a843bb1038ca4e236558ac85fcec88
Ubuntu <ubuntu@ip-172-31-44-227.us-west-2.compute.internal>
2016-03-17 17:25:44 +0000

    make it better

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
-var express = require('express')
-var app1 = express()
+var fs = require('fs')
 
-/* serves all the static files*/
-app1.set('port', (process.env.PORT || 8080))
-app1.use(express.static(__dirname + '/public'))
+var data = fs.readFileSync('index.html')
 
-/*serves main page*/
-app1.get('/', function(request, response) {
-response.sendfile('index.html')
-})
-
-app1.listen(app1.get('port'), function() {
-  console.log("Node app is running at localhost:" + app1.get('port'))
+data.listen(8080, function() {
+  console.log("Node app is running at localhost:" + 8080)
 })
 
