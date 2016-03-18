3e0971596fc46d64e01a9ea79440d5d7686b6f22
Ubuntu <ubuntu@ip-172-31-44-227.us-west-2.compute.internal>
2016-03-18 03:57:11 +0000

    finish part 1 a

--- index.js
+++ index.js
@@ -1,26 +1,13 @@
 var fs = require('fs')
 var http = require('http')
-var buf = new Buffer(2490);
 
-function readHtml(callback) {
-  fs.open("index.html", 'r+', function(err, fd)       
-  {
-    if (err) return callback(err)
-    
-    fs.read(fd, buf, 0, buf.length, 0, function(err, bytes)
-    {
-       if (err) return callback(err)
-       callback(null, buf.slice(0, bytes))         
-    })
-  })
+var data = fs.readFileSync('index.html')
+
+function onRequest(request, response) {
+  response.writeHead(200, {"Content-Type": "html"})
+  response.write(data)
+  response.end()
 }
 
-readHtml(function(err, content) {
-  function onRequest(request, response) {
-    response.writeHead(200, {"Content-Type": "html"})
-    response.write(content)
-    response.end()
-  }
-  http.createServer(onRequest).listen(process.env.PORT || 8080)
-})
+http.createServer(onRequest).listen(process.env.PORT || 8080)
 
