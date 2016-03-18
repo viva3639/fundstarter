3d4eab2fe368b53da54459e9eb93eb263d1ce445
Ubuntu <ubuntu@ip-172-31-44-227.us-west-2.compute.internal>
2016-03-18 03:30:56 +0000

    make it better

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
-var fs = require("fs")
-var buf = new Buffer(2490)
+var fs = require('fs')
+var http = require('http')
 
-fs.open('index.html', 'r+', function(err, fd) {
-   if (err) {
-       return console.error(err)
-   }   
-   fs.read(fd, buf, 0, buf.length, 0, function(err, bytes){
-      if (err){
-         console.log(err)
-      }      
-      if(bytes > 0){
-         
-         function onRequest(request, response) {
-           response.writeHead(200, {"Content-Type": "html"})
-           response.write(buf.slice(0, bytes))
-           response.end()
-         }
-         http.createServer(onRequest).listen(process.env.PORT || 8080)
-      }
-   })
+function readHtml(callback) {
+  fs.readFile("index.html", "utf8", function (err, content) {
+    if (err) return callback(err)
+    callback(null, content)
+  })
+}
+
+readHtml(function(err, content) {
+  function onRequest(request, response) {
+    response.writeHead(200, {"Content-Type": "html"})
+    response.write(content)
+    response.end()
+  }
+  http.createServer(onRequest).listen(process.env.PORT || 8080)
 })
-
-
