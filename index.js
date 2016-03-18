c78ff8cc7682cc11d89599d9957b90a1c2b3c71e
Ubuntu <ubuntu@ip-172-31-44-227.us-west-2.compute.internal>
2016-03-18 02:23:26 +0000

    make it better

--- index.js
+++ index.js
@@ -2,21 +2,18 @@ var fs = require('fs')
 var http = require('http')
 
 function readHtml(callback) {
-  fs.readFile("index.html", function read(err, content) {
+  fs.readFile("index.html", "utf8", function read(err, content) {
     if (err) return callback(err)
     callback(null, content)
   })
 }
 
-var data = readHtml(function(err, content) {
-   return content
+readHtml(function(err, content) {
+  function onRequest(request, response) {
+    response.writeHead(200, {"Content-Type": "html"})
+    response.write(content)
+    response.end()
+  }
+  http.createServer(onRequest).listen(process.env.PORT || 8080)
 })
-
-function onRequest(request, response) {
-  response.writeHead(200, {"Content-Type": "html"})
-  response.write(data)
-  response.end()
-}
-
-http.createServer(onRequest).listen(process.env.PORT || 8080)
 
