b549ecf16c533ad174f5c771d734f3b944e46613
Ubuntu <ubuntu@ip-172-31-44-227.us-west-2.compute.internal>
2016-03-18 04:03:30 +0000

    Finish part 2

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 var fs = require('fs')
 var http = require('http')
+var buf = new Buffer(2490);
 
-var data = fs.readFileSync('index.html')
-
-function onRequest(request, response) {
-  response.writeHead(200, {"Content-Type": "html"})
-  response.write(data)
-  response.end()
+function readHtml(callback) {
+  fs.open("index.html", 'r+', function(err, fd)       
+  {
+    if (err) return callback(err)
+    
+    fs.read(fd, buf, 0, buf.length, 0, function(err, bytes)
+    {
+       if (err) return callback(err)
+       callback(null, buf.slice(0, bytes))         
+    })
+  })
 }
 
-http.createServer(onRequest).listen(process.env.PORT || 8080)
+readHtml(function(err, content) {
+  function onRequest(request, response) {
+    response.writeHead(200, {"Content-Type": "html"})
+    response.write(content)
+    response.end()
+  }
+  http.createServer(onRequest).listen(process.env.PORT || 8080)
+})
 
